12631046d35f911c67dce15b9a969b4e0c9aadde
Traineau <nathan.trn@orange.fr>
2019-11-17 15:44:26 +0100

    Added course and download page

---
+++ src/pages/CoursePage.js
@@ -0,0 +1,68 @@
+import React from 'react';
+import { StyleSheet, Text, View, FlatList } from 'react-native';
+
+export default class CoursePage extends React.Component {
+    
+    render() {
+        const data = {characters: [
+            {id:123, name:'Sheldon', profession:'Theoretical Physicist'},
+            {id:234, name:'Leonard', profession:'Experimental Physicist'},
+            {id:345, name:'Howard', profession:'Mechanical Engineer'},
+            {id:456, name:'Raj', profession:'Astro-Physicist'},
+            {id:567, name:'Amy', profession:'Neurobiologist'},
+            {id:678, name:'Bernadette', profession:'Microbiologist'},
+        ]};
+        const list = [{key:'Sheldon', prof:'Theoretical Physicist'},
+                     {key:'Leonard', prof:'Experimental Physicist'},
+                     {key:'Howard', prof:'Mechanical Engineer'},
+                     {key:'Raj', prof:'Astro-Physicist'},
+                     {key:'Amy', prof:'Neurobiologist'},
+                     {key:'Bernadette', prof:'Microbiologist'},
+                     {key:'Penny', prof:'Actress'},
+                     {key:'Dr. Gabelhauser', prof:'Dean'},
+                     {key:'Stuart', prof:'comic store owner'},
+                     {key:'Barry', prof:'Engineer'},
+                     {key:'Leslie', prof:'Physicist'},
+                     {key:'Priya', prof:'Lawyer'},
+                     {key:'Lucy', prof:'unknown'}];
+        return (
+            <View style={styles.container}>
+            
+            { data.characters.length > 0 ? 
+                <FlatList data={data.characters}
+                    renderItem={({item})=> (
+                <View style={{borderBottomColor:'#999', padding:10}}>
+            <Text style={{fontSize:60, fontWeight:'bold', color:'#333'}}>
+                {item.id}
+            </Text>
+            <Text style={{fontSize:36, color:'#999'}}>
+                {item.profession}
+            </Text>
+        </View>
+                    
+            )} />
+            
+            :
+                <Text style={{fontSize: 48, color:'red'}}>
+                    Sorry. No Videos Available.
+                </Text>
+            }
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+  container: {
+      backgroundColor: '#fff',
+      paddingTop: 20,
+      paddingBottom: 20
+  },
+    listitem: {
+        fontSize:48, 
+        fontWeight:'bold', 
+        color:'#333',
+        borderBottomWidth: 2,
+        borderBottomColor: '#999'
+    }
+});
