e2d7b83ede28626842c093ecc26047579423e352
Yvan Sanson <yvan.sanson@etu.umontpellier.fr>
2019-11-20 16:48:01 +0100

    Minor typo error on deleted button + refactor

--- src/components/DeleteDownloadedVideoButton.js
+++ src/components/DeleteDownloadedVideoButton.js
@@ -1,39 +1,43 @@
 import React from 'react';
-import {Button} from 'react-native';
+import {Button, Alert} from 'react-native';
 import videoCaching from '../utils/videoCaching';
 import {colors} from '../styles/base';
 
 export default class DeleteDownloadedVideoButton extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            buttonTitle: 'Delete',
-        };
-        this.deleteVideo = this.deleteVideo.bind(this);
+  constructor(props) {
+    super(props);
+    this.state = {
+      buttonTitle: 'Delete',
     };
+    this.deleteVideo = this.deleteVideo.bind(this);
+  }
 
-    deleteVideo =() => {
-        videoCahing.deleteVideo(this.props.video, this.props.subtitles)
-            .then(result => {
-                if(result){
-                    this.props.deleteVideo(this.props.video, this.props.subtitles);
-                }
-                Alert.alert('Success', 'Deleted!');
-            })
-            .catch(err => {
-                console.log(err);
-                Alert.alert('Error', 'Problem in te video deletion');
+  deleteVideo = () => {
+    videoCaching
+      .deleteVideo(this.props.video, this.props.subtitles)
+      .then(result => {
+        if (result) {
+          this.props.deleteVideo(
+            this.props.video,
+            this.props.subtitles,
+            this.props.course,
+          );
+        }
+        Alert.alert('Success', 'Deleted!');
+      })
+      .catch(err => {
+        console.log(err);
+        Alert.alert('Error', 'Problem in te video deletion');
+      });
+  };
 
-            });
-    }
-
-    render() {
-        return (
-            <Button
-                title={this.state.buttonTitle}
-                onPress={this.deleteVideo}
-                color={colors.purple}
-            />
-        );
-    }
+  render() {
+    return (
+      <Button
+        title={this.state.buttonTitle}
+        onPress={this.deleteVideo}
+        color={colors.purple}
+      />
+    );
+  }
 }
--- src/pages/DownloadedVideos.js
+++ src/pages/DownloadedVideos.js
@@ -5,44 +5,43 @@ import Orientation from 'react-native-orientation';
 import DeletionButton from '../containers/DeleteVideoContainer';
 
 export default class CoursePage extends React.Component {
-
   componentDidMount() {
     Orientation.lockToPortrait();
   }
 
   render() {
     const data = [
-        {
-          courseName: 123,
-          videoName: 'Sheldon',
-          subtitlesName: 'Theoretical Physicist',
-        },
-        {
-          courseName: 234,
-          videoName: 'Leonard',
-          subtitlesName: 'Experimental Physicist',
-        },
-        {
-          courseName: 345,
-          videoName: 'Howard',
-          subtitlesName: 'Mechanical Engineer',
-        },
-        {
-          courseName: 456,
-          videoName: 'Raj',
-          subtitlesName: 'Astro-Physicist',
-        },
-        {
-          courseName: 567,
-          videoName: 'Amy',
-          subtitlesName: 'Neurobiologist',
-        },
-        {
-          courseName: 678,
-          videoName: 'Bernadette',
-          subtitlesName: 'Microbiologist',
-        },
-      ];
+      {
+        courseName: 123,
+        videoName: 'Sheldon',
+        subtitlesName: 'Theoretical Physicist',
+      },
+      {
+        courseName: 234,
+        videoName: 'Leonard',
+        subtitlesName: 'Experimental Physicist',
+      },
+      {
+        courseName: 345,
+        videoName: 'Howard',
+        subtitlesName: 'Mechanical Engineer',
+      },
+      {
+        courseName: 456,
+        videoName: 'Raj',
+        subtitlesName: 'Astro-Physicist',
+      },
+      {
+        courseName: 567,
+        videoName: 'Amy',
+        subtitlesName: 'Neurobiologist',
+      },
+      {
+        courseName: 678,
+        videoName: 'Bernadette',
+        subtitlesName: 'Microbiologist',
+      },
+    ];
 
     return (
       <View style={styles.container}>
@@ -57,7 +56,11 @@ export default class CoursePage extends React.Component {
                 <Text style={{fontSize: 36, color: '#999'}}>
                   {item.videoName}
                 </Text>
-                <DeletionButton video={item.videoName} subtitles={item.subtitlesName} />
+                <DeletionButton
+                  video={item.videoName}
+                  subtitles={item.subtitlesName}
+                  course={item.courseName}
+                />
               </View>
             )}
           />
